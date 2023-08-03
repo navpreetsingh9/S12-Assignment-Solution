87620b2e7ebfb23278fd4771209d4b7788d5898f
Navpreet Singh <navpreet_np@yahoo.com>
2023-08-03 15:26:51 -0400

    Update utils.py

--- cifar10/utils.py
+++ cifar10/utils.py
@@ -5,27 +5,68 @@ from typing import Union
 import torch
 from torch import nn
 from torchinfo import summary
-import torchvision
+import torch.nn.functional as F
 
-def get_lr(optimizer):
-    """
-    Tracking learning rate during training
-    """
-    for param_group in optimizer.param_groups:
-        return param_group['lr']
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
 def imshow(img):
+    """
+    Display image
+
+
+    Args:
+        img: Given image to display
+    """
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-def show_images(img_loader, classes):
-    # get some random training images
-    dataiter = iter(img_loader)
-    images, labels = next(dataiter)
-    imshow(torchvision.utils.make_grid(images[:8]))
-    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+def denormalize_image(image, mean, std):
+    """
+    Denormalize an image given its mean and standard deviation.
+
+    Parameters:
+        image (torch.Tensor): The input image tensor to denormalize.
+        mean (tuple or list): The mean used for normalization.
+        std (tuple or list): The standard deviation used for normalization.
+
+    Returns:
+        torch.Tensor: The denormalized image tensor.
+    """
+    # Convert the mean and std to tensors
+    mean_tensor = torch.tensor(mean).reshape(1, -1, 1, 1)
+    std_tensor = torch.tensor(std).reshape(1, -1, 1, 1)
+
+    # Denormalize the image
+    denormalized_image = image * std_tensor + mean_tensor
+
+    return denormalized_image
+
+def show_images(img_loader, class_map, count=10):
+    """
+    Display multiple images
+    
+
+    Args:
+        img_loader (Dataloader): dataloader for training data
+        classes (int, optional): Number of images to show. Defaults to 10.
+    """
+    # Print Random Samples
+    if not count % 10 == 0:
+        return
+
+    classes = list(class_map.keys())
+    fig = plt.figure(figsize=(15, 5))
+    for imgs, labels in img_loader:
+        for i in range(count):
+            ax = fig.add_subplot(int(count / 10), 10, i + 1, xticks=[], yticks=[])
+            ax.set_title(f"{classes[labels[i]]}")
+            plt.imshow(np.clip(imgs[i], 0, 1).cpu().numpy().transpose(1, 2, 0).astype(float))
+        break
+    plt.show()
 
     
 def print_summary(model, input_size=(1, 28, 28)):
@@ -60,7 +101,7 @@ def load_weights_from_path(model, path):
     model.load_state_dict(torch.load(path))
     return model
 
-def get_incorrrect_predictions(model, loader, device):
+def get_incorrect_predictions(model, loader, device):
     """Get all incorrect predictions
 
     Args:
@@ -145,3 +186,76 @@ def prepare_confusion_matrix(all_preds, all_targets, class_map):
         confusion_matrix[tl, pl] = confusion_matrix[tl, pl] + 1
 
     return confusion_matrix
+
+def plot_incorrect_predictions(predictions, class_map, count=10):
+    """Plot Incorrect predictions
+
+    Args:
+        predictions (list): List of all incorrect predictions
+        class_map (dict): Lable mapping
+        count (int, optional): Number of samples to print, multiple of 5. Defaults to 10.
+    """
+    print(f'Total Incorrect Predictions {len(predictions)}')
+
+    if not count % 10 == 0:
+        print("Count should be multiple of 10")
+        return
+
+    classes = list(class_map.keys())
+
+    fig = plt.figure(figsize=(15, 5))
+    fig.suptitle("Correct_Label/Incorrect_Prediction", fontsize=16, fontweight="bold")
+    for i, (d, t, p, o) in enumerate(predictions):
+        ax = fig.add_subplot(int(count/10), 10, i + 1, xticks=[], yticks=[])
+        ax.set_title(f'{classes[t.item()]}/{classes[p.item()]}')
+        plt.imshow(np.clip(d, 0, 1).cpu().numpy().transpose(1, 2, 0))
+        if i+1 == count:
+            break
+    plt.show()
+    
+
+def plot_network_performance(epochs, train_losses, test_losses, train_acc, test_acc):
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.plot(range(epochs), train_losses, 'g', label='Training loss')
+    plt.plot(range(epochs), test_losses, 'b', label='Validation loss')
+    plt.title('Training and Validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(1, 3, 2)
+    plt.plot(range(epochs), train_acc, 'g', label='Training Accuracy')
+    plt.plot(range(epochs), test_acc, 'b', label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+def show_gradcam(model, incorrect, class_map, use_cuda, mean, std, count=10):
+    target_layers  = [model.layer4[-1]]
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
+
+    if not count % 10 == 0:
+        return
+
+    classes = list(class_map.keys())
+    fig = plt.figure(figsize=(15, 5))
+    fig.suptitle("Correct_Label/Incorrect_Prediction", fontsize=16, fontweight="bold")
+    for i, (imgs, labels, pred, output) in enumerate(incorrect):
+        input_tensor = imgs.unsqueeze(0)
+        rgb_img = denormalize_image(input_tensor, mean, std).transpose(3, 1).numpy()[0]
+        grayscale_cam = cam(input_tensor=input_tensor)
+        grayscale_cam = grayscale_cam[0, :]
+        visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+        ax = fig.add_subplot(int(20 / 10), 10, i + 1, xticks=[], yticks=[])
+        ax.set_title(f'{classes[labels.item()]}/{classes[pred.item()]}')
+        plt.imshow(visualization)
+        if i+1 == count:
+            break
+    plt.show()
